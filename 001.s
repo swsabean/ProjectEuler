ec7497f83715878cc3270250cf5c461e3159c1bb
swsabean <swsabean@yahoo.com>
2023-12-05 07:39:26 -0600

    Update 001.s

--- 001.s
+++ 001.s
@@ -6,7 +6,7 @@ sum:     .word 0
 
 _start:
     // Calculate number of multiples
-    ldr x0, =999      // Upper limit - 1 (since we want below 1000)
+    mov x0, #999      // Upper limit - 1 (since we want below 1000)
     mov x1, 3         // Divisor for 3
     mov x2, 5         // Divisor for 5
     mov x3, 15        // Divisor for 15

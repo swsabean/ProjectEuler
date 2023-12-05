c181ed8a8ac3252b32fd88b20ab55cf3f8efa162
swsabean <swsabean@yahoo.com>
2023-12-05 07:10:15 -0600

    Update 001.s

--- 001.s
+++ 001.s
@@ -1,2 +1,13 @@
 .section .data
-sum:    .word 0
+sum:     .word 0
+
+.section		.text
+.global _start
+
+_start:
+    // Calculate number of multiples
+    ldr x0, =999      // Upper limit - 1 (since we want below 1000)
+    mov x1, 3         // Divisor for 3
+    mov x2, 5         // Divisor for 5
+    mov x3, 15        // Divisor for 15
+
